0dae9d34512f7637415ee20205c47705d35f9b46
Parasrah <git@parasrah.com>
2019-08-15 16:37:29 -0600

    emit node.js bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const root = (...filesOrDirs) => path.resolve('.', ...filesOrDirs)
 
@@ -6,18 +7,26 @@ const src = (...filesOrDirs) => path.join(root('src'), ...filesOrDirs)
 
 const dist = (...filesOrDirs) => path.join(root('dist'), ...filesOrDirs)
 
-module.exports = () => ({
-  entry: src('index.js'),
-  mode: 'production',
-  output: {
-    path: dist(),
-    filename: 'maybe.js',
-    library: 'maybe',
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+function createConfig (target) {
+  return {
+    entry: src('index.js'),
+    mode: 'production',
+    target,
+    output: {
+      path: dist(),
+      filename: `maybe.${target}.js`,
+      library: 'maybe'
+      libraryTarget: 'umd'
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin()
     ]
   }
-})
+}
+
+module.exports = [createConfig('node'), createConfig('web')]
